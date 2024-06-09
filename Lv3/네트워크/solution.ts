496929fe8c1bb1df7c5058f4918207073aeb1855
likeprograming1 <hsj2216@naver.com>
2024-06-09 15:24:24 +0900

    3레벨 네트워크 완료

--- Lv3/네트워크/solution.ts
+++ Lv3/네트워크/solution.ts
@@ -1,12 +1,27 @@
-export default function solution(n: number, computers: number[][]) {
-  var result = 0;
+export default function solution(n: number, computers: number[][]): number {
+  // 방문 여부를 추적하는 배열
+  const visited: boolean[] = new Array(n).fill(false);
 
-  for (var i = 0; i < computers.length; i++) {
-    var sum = computers[i].reduce((acc, cur) => {
-      return acc + cur;
-    });
-    result += sum;
+  // DFS 함수 정의
+  const dfs = (node: number) => {
+    visited[node] = true;
+    for (let i = 0; i < n; i++) {
+      // 노드가 연결되어 있고 방문하지 않은 경우 DFS 재귀 호출
+      if (computers[node][i] === 1 && !visited[i]) {
+        dfs(i);
+      }
+    }
+  };
+
+  // 네트워크 개수를 추적하는 변수
+  let networkCount = 0;
+  for (let i = 0; i < n; i++) {
+    // 방문되지 않은 노드 발견 시 DFS 시작
+    if (!visited[i]) {
+      dfs(i);
+      networkCount++;
+    }
   }
 
-  return n - result;
+  return networkCount;
 }
